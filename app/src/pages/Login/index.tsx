d84d4c11b9d51a603d0afabc8741b18213a5e0c9
pedrobe30 <pedrobernardo.ribeiro@gmail.com>
2025-02-26 18:08:51 -0300

    Subindo o Projeto

---
+++ app/src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+
+export default function Login() {
+ return (
+   <View>
+    <Text>
+        OLAAAAAAAAA
+    </Text>
+   </View>
+  );
+}
