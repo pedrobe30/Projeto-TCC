fb742aba275d99f42777e5c81b326c48897650ed
Vinicius Lima <vinicius.nasc.2007@gmail.com>
2025-04-30 15:58:36 -0300

    Criei a tela Perfil e fiz adições no app.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Autenticacao from './app/src/pages/Autenticacao';
 import Login from './app/src/pages/Login';
 import Home from './app/src/pages/Home';
+//import Perfil from './app/src/pages/Perfil';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
         <Stack.Screen name="Autenticacao" component={Autenticacao} />
         <Stack.Screen name='Login' component={Login} />
         <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Perfil' component={Perfil} />
       </Stack.Navigator>
     </NavigationContainer>
   );
